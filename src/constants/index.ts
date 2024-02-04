b5f4fe3b29895c9ee74cf810c147c3a4a3d0eea1
bombasters <bruni.glat@yandex.ru>
2024-02-04 23:27:30 +0300

    constants file add

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const constants = {
+  queryKeys: {
+    GET_TASKS: "tasks",
+    GET_PROFILE: "get-me",
+    SUBDIVISIONS: "subdivisions",
+    DELETE_TASKS: "tasks-delete",
+    CHANGE_TASKS: "tasks-change",
+    INVITES: "invites",
+    MY_ITEMS: "my-items",
+    SHOP: "SHOP",
+    SEND_INVITES: "send-invites",
+    USE_ITEMS: "use-items",
+  },
+};
